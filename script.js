290946676423330c31aea81d0cc0dce2ba4c4dc0
johnny <jhuang007007@gmail.com>
2022-03-09 13:41:31 -0500

    all number buttons when clicked or pressed on the keyboard will show up in the display. clear button clears the display

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
 function clear() {
-
+  const displaycalc = document.querySelector('.displaycalc');
+  const clearbutton = document.querySelector('.container .clear');
+  clearbutton.addEventListener('click', () => {
+    displaycalc.textContent = '';
+  });
 }
 
-function equals() {
+function operate(operator, a, b) {
   
 }
+
+function displayCalculation() {
+  const displaycalc = document.querySelector('.displaycalc');
+  const numberbuttons = document.querySelectorAll('.container .number');
+  numberbuttons.forEach((numberbutton) => {
+    numberbutton.addEventListener('click', () => {
+      displaycalc.textContent += numberbutton.textContent;
+    });
+  });
+}
+
+function displayCalculationHotKeys() {
+  document.addEventListener('keydown', getNumberFromKeyEvent);
+}
+
+function getNumberFromKeyEvent(event) {
+  const displaycalc = document.querySelector('.displaycalc');
+  //numpad number keys 0-9
+  if (event.keyCode >= 96 && event.keyCode <= 105) {
+    displaycalc.textContent += event.keyCode - 96;
+    //normal number keys 0-9
+  } else if (event.keyCode >= 48 && event.keyCode <= 57) {
+    displaycalc.textContent += event.keyCode - 48;
+  }
+  return null;
+}
+
+displayCalculationHotKeys();
+displayCalculation();
+clear();
